8b4d5b7fb1b779cdd6de4cf35c2c8d3220e28cf2
Yevheniia Tobolovska <tobolovskaya@gmail.com>
2024-12-17 09:54:22 +0100

    1

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,12 @@
-# Використовуємо офіційний образ Python
+
 FROM python:3.11-slim
 
-# Встановлюємо робочу директорію в контейнері
 WORKDIR /app
 
-# Копіюємо файл залежностей
-COPY requirements.txt .
-
-# Встановлюємо залежності
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Копіюємо всі файли проекту в контейнер
 COPY . .
 
-# Відкриваємо порт 3000 для додатку
 EXPOSE 3000
 
-# Команда для запуску додатку
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -5,10 +5,8 @@ import os
 
 app = Flask(__name__)
 
-# Шлях до файлу для збереження повідомлень
 DATA_FILE = os.path.join("storage", "data.json")
 
-# Створіть storage/data.json, якщо його немає
 if not os.path.exists("storage"):
     os.makedirs("storage")
 
@@ -17,13 +15,11 @@ if not os.path.exists(DATA_FILE):
         json.dump({}, file)
 
 
-# Головна сторінка
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-# Сторінка для надсилання повідомлення
 @app.route("/message", methods=["GET", "POST"])
 def message():
     if request.method == "POST":
@@ -31,7 +27,7 @@ def message():
         message = request.form.get("message")
 
         if username and message:
-            # Збереження у файл
+            
             with open(DATA_FILE, "r") as file:
                 data = json.load(file)
 
@@ -45,7 +41,6 @@ def message():
     return render_template("message.html")
 
 
-# Сторінка для читання збережених повідомлень
 @app.route("/read")
 def read():
     with open(DATA_FILE, "r") as file:
@@ -53,13 +48,11 @@ def read():
     return render_template("read.html", messages=messages)
 
 
-# Маршрут для статичних ресурсів (CSS, лого)
 @app.route("/<path:filename>")
 def static_files(filename):
     return send_from_directory("static", filename)
 
 
-# Обробка помилки 404
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("error.html"), 404
